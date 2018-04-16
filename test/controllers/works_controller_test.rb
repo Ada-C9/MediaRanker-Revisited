2e706d213653b967993b5259b47fb2635b0891f3
Caroline Nardi <caroline.m.nardi@gmail.com>
2018-04-16 15:39:00 -0700

    Added controller tests for works#create

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -108,11 +108,17 @@ describe WorksController do
 
   describe "show" do
     it "succeeds for an extant work ID" do
+      work_id = Work.first.id
 
+      get work_path(work_id)
+      must_respond_with :success
     end
 
     it "renders 404 not_found for a bogus work ID" do
+      work_id = Work.last.id + 1
 
+      get work_path(work_id)
+      must_respond_with :not_found
     end
   end
 
