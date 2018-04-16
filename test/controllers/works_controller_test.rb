e07c6eb62782dd869bd9c6bc77936235a8dbf907
Hannah Cameron <hannahiscoding@gmail.com>
2018-04-16 15:50:00 -0700

    passing all detroy tests

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -191,16 +191,30 @@ describe WorksController do
       must_respond_with :not_found
     end
   end
-  #
-  # describe "destroy" do
-  #   it "succeeds for an extant work ID" do
-  #
-  #   end
-  #
-  #   it "renders 404 not_found and does not update the DB for a bogus work ID" do
-  #
-  #   end
-  # end
+
+  describe "destroy" do
+    it "succeeds for an extant work ID" do
+      work_id = Work.first.id
+      work_count = Work.count
+
+      delete work_path(work_id)
+
+      must_respond_with :redirect
+      must_redirect_to root_path
+
+      Work.count.must_equal work_count - 1
+      Work.find_by(id: work_id).must_be_nil
+    end
+
+    it "renders 404 not_found and does not update the DB for a bogus work ID" do
+      work_id = Work.last.id + 1
+      work_count = Work.count
+
+      delete work_path(work_id)
+      must_respond_with :not_found
+      Work.count.must_equal work_count
+    end
+  end
   #
   # describe "upvote" do
   #
