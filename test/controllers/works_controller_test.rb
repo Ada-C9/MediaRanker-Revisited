b674b1eed9acb5c30896c16dbeba9f21d1ffd12c
Mary Lamkin <mmlamkin@gmail.com>
2018-04-16 15:57:32 -0700

    adds passing tests for upvote

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -173,6 +173,7 @@ describe WorksController do
     it "succeeds for an extant work ID" do
       delete work_path(Work.find(works(:movie).id))
       must_respond_with :redirect
+      must_redirect_to root_path
     end
 
     it "renders 404 not_found and does not update the DB for a bogus work ID" do
@@ -184,18 +185,29 @@ describe WorksController do
   describe "upvote" do
 
     it "redirects to the work page if no user is logged in" do
-
+      post upvote_path(Work.find(works(:movie).id))
+      must_respond_with :redirect
+      must_redirect_to work_path(Work.find(works(:movie).id))
     end
 
     it "redirects to the work page after the user has logged out" do
-
+    skip
     end
 
     it "succeeds for a logged-in user and a fresh user-vote pair" do
+      get login_path
+      Vote.count.must_equal 3
+      post upvote_path(Work.find(works(:movie).id))
+      must_respond_with :redirect
+      must_redirect_to work_path(Work.find(works(:movie).id))
 
     end
 
     it "redirects to the work page if the user has already voted for that work" do
+      get login_path
+      post upvote_path(Work.find(works(:movie).id))
+      post upvote_path(Work.find(works(:movie).id))
+      must_redirect_to work_path(Work.find(works(:movie).id))
 
     end
   end
