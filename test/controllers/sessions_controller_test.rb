98722028d004250d1ee7431a48ee603b57a772da
Lily Sky <lilyxsky@gmail.com>
2018-04-16 15:42:45 -0700

    stubs

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,5 +1,24 @@
 require "test_helper"
 
 describe SessionsController do
+  describe "login" do
+    it "succeeds with new user" do
+      User.login
+    end
 
+    it "succeeds with existing user" do
+
+    end
+
+    it "succeeds with no media" do
+
+    end
+  end
+
+  describe "logout" do
+    it "succeeds" do
+
+
+    end
+  end
 end
