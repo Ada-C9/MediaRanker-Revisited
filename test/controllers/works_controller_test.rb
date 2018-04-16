b97ada46ad09e54c8f98a35bcd69013519e5f15f
Alexandria Brown <axbn2190@gmail.com>
2018-04-16 16:21:27 -0700

    works tests for show and edit

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -4,14 +4,12 @@ require 'pry'
 describe WorksController do
   describe "root" do
     it "succeeds with all media types" do
-      # Precondition: there is at least one media of each category
       get root_path
 
       must_respond_with :success
     end
 
     it "succeeds with one media type absent" do
-      # Precondition: there is at least one media in two of the categories
       Work.where(category: "album").destroy_all
 
       get root_path
@@ -33,14 +31,25 @@ describe WorksController do
 
   describe "index" do
     it "succeeds when there are works" do
+      get works_path
+
+      must_respond_with :success
     end
 
     it "succeeds when there are no works" do
+      Work.destroy_all
+
+      get works_path
+
+      must_respond_with :success
     end
   end
 
   describe "new" do
     it "succeeds" do
+      get new_work_path
+
+      must_respond_with :success
     end
   end
 
@@ -60,50 +69,53 @@ describe WorksController do
     end
 
     it "renders bad_request and does not update the DB for bogus data" do
-        post works_path, params: {
-          work: {
-            title: nil,
-            category: "album"
-          }
+      post works_path, params: {
+        work: {
+          title: nil,
+          category: "album"
         }
+      }
 
       must_respond_with :bad_request
     end
 
     it "renders 400 bad_request for bogus categories" do
       post works_path, params: {
-        two: {
+        work: {
           title: "serious",
           category: "photo"
         }
       }
 
-    must_respond_with :bad_request
+      must_respond_with :bad_request
     end
-
   end
 
   describe "show" do
     it "succeeds for an extant work ID" do
-      get work_path works(:album).id
+      get work_path(Work.first.id)
 
       must_respond_with :success
     end
 
     it "renders 404 not_found for a bogus work ID" do
-      # get work_path works(:fake).id
-      #
-      # must_respond_with :missing
+      get work_path(" ")
+
+      must_respond_with :not_found
     end
   end
 
   describe "edit" do
     it "succeeds for an extant work ID" do
+      get edit_work_path(Work.last.id)
 
+      must_respond_with :success
     end
 
     it "renders 404 not_found for a bogus work ID" do
+      get edit_work_path(" ")
 
+      must_respond_with :not_found
     end
   end
 
