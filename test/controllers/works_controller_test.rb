73bcdba348a4da64c4374ae73d92b836f52e1b29
LetiTran <leticiastran@gmail.com>
2018-04-16 21:06:18 -0700

    Added tests for works_controller#update

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -62,109 +62,130 @@ describe WorksController do
             title: "New Work",
             category:  "album"
           }
+        }
+      }.must_change 'Work.count', 1
+
+      # Assert
+      must_respond_with :redirect
+      must_redirect_to work_path(Work.last.id)
+
+
+    end
+
+    it "renders not_found and does not update the DB for bogus data" do
+      proc {
+        post works_path, params: {
+          work: {
+            title: "New work"
+            # missing category
           }
-        }.must_change 'Work.count', 1
+        }
+      }.must_change 'Work.count', 0
 
-        # Assert
-        must_respond_with :redirect
-        must_redirect_to work_path(Work.last.id)
+      must_respond_with :error
+    end
 
+    it "renders 400 bad_request for bogus categories" do
+      proc {
+        post works_path, params: {
+          work: {
+            title: "New work",
+            category:  "wrong category"
+          }
+        }
+      }.must_change 'Work.count', 0
 
-      end
+      must_respond_with 400
+    end
 
-      it "renders not_found and does not update the DB for bogus data" do
-        proc {
-             post works_path, params: {
-               work: {
-                 title: "New work"
-                 # missing category
-               }
-             }
-           }.must_change 'Work.count', 0
+  end
 
-           must_respond_with :error
-      end
+  describe "show" do
+    it "succeeds for an extant work ID" do
+      get work_path(works(:album).id)
 
-      it "renders 400 bad_request for bogus categories" do
-        proc {
-             post works_path, params: {
-               work: {
-                 title: "New work",
-                category:  "wrong category"
-               }
-             }
-           }.must_change 'Work.count', 0
-
-           must_respond_with 400
-      end
+      must_respond_with :success
 
     end
 
-    describe "show" do
-      it "succeeds for an extant work ID" do
-        get work_path(works(:album).id)
+    it "renders 404 not_found for a bogus work ID" do
+      get work_path("wrong id")
 
-        must_respond_with :success
-
-      end
-
-      it "renders 404 not_found for a bogus work ID" do
-        get work_path("wrong id")
-
-        must_respond_with :missing
-      end
-    end
-
-    describe "edit" do
-      it "succeeds for an extant work ID" do
-
-      end
-
-      it "renders 404 not_found for a bogus work ID" do
-
-      end
-    end
-
-    describe "update" do
-      it "succeeds for valid data and an extant work ID" do
-
-      end
-
-      it "renders bad_request for bogus data" do
-
-      end
-
-      it "renders 404 not_found for a bogus work ID" do
-
-      end
-    end
-
-    describe "destroy" do
-      it "succeeds for an extant work ID" do
-
-      end
-
-      it "renders 404 not_found and does not update the DB for a bogus work ID" do
-
-      end
-    end
-
-    describe "upvote" do
-
-      it "redirects to the work page if no user is logged in" do
-
-      end
-
-      it "redirects to the work page after the user has logged out" do
-
-      end
-
-      it "succeeds for a logged-in user and a fresh user-vote pair" do
-
-      end
-
-      it "redirects to the work page if the user has already voted for that work" do
-
-      end
+      must_respond_with :missing
     end
   end
+
+  describe "edit" do
+    it "succeeds for an extant work ID" do
+      get edit_work_path(works(:album).id)
+
+      must_respond_with :success
+    end
+
+    it "renders 404 not_found for a bogus work ID" do
+      get edit_work_path("wrong id")
+
+      must_respond_with :missing # 404
+    end
+  end
+
+  describe "update" do
+    it "succeeds for valid data and an extant work ID" do
+
+      patch work_path(works(:album).id), params: {
+        work: {
+          title: "Update title"
+        }
+      }
+
+      must_respond_with :redirect
+      must_redirect_to work_path(works(:album).id)
+    end
+
+    it "renders bad_request for bogus data" do
+
+      patch work_path(works(:album).id), params: {
+        work: {
+          doesnt_exists: "Update title"
+        }
+      }
+
+      must_respond_with :redirect
+    end
+
+    it "renders 404 not_found for a bogus work ID" do
+      patch work_path("wrong id")
+
+      must_respond_with :missing # 404
+    end
+  end
+
+  describe "destroy" do
+    it "succeeds for an extant work ID" do
+
+    end
+
+    it "renders 404 not_found and does not update the DB for a bogus work ID" do
+
+    end
+  end
+
+  describe "upvote" do
+
+    it "redirects to the work page if no user is logged in" do
+
+    end
+
+    it "redirects to the work page after the user has logged out" do
+
+    end
+
+    it "succeeds for a logged-in user and a fresh user-vote pair" do
+
+    end
+
+    it "redirects to the work page if the user has already voted for that work" do
+
+    end
+  end
+end
