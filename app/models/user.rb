5b010f2d344e606ea27c135e394ce9c974e6e73e
Kat <kat.eastman@gmail.com>
2018-04-17 12:41:08 -0700

    non-functional, commiting before drastic changes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,24 @@
+require 'pry'
 class User < ApplicationRecord
   has_many :votes
   has_many :ranked_works, through: :votes, source: :work
 
   validates :username, uniqueness: true, presence: true
 
-def build_from_github(auth_hash)
+def self.build_from_github(auth_hash)
 user_data = {
   uid: auth_hash[:uid],
-  name: auth_hash["info"]["name"],
+  username: auth_hash["info"]["name"],
   email: auth_hash["info"]["email"],
   provider: auth_hash[:provider]
 }
 
-  user = self.New(user_data)
-  return user if user.save
+  user = self.new(user_data)
+  if user.save
+    return user
+  else
+    binding.pry
+  end
 end
 
 end
