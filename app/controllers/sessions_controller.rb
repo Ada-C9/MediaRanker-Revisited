5cb68f9ce44baa94179cdb12fb9fa7c8c6f9e56d
Dikla <dikla.aharoni@gmail.com>
2018-04-17 12:30:07 -0700

    Added migratione file Added route callback Added create method to user controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,38 @@ class SessionsController < ApplicationController
   def login_form
   end
 
+  def create
+    auth_hash = request.env['omniauth.auth']
+
+    if auth_hash['uid']
+      @user = User.find_by(uid: auth_hash[:uid], provider: 'github')
+      if @user.nil?
+        # User doesn't match anything in the DB
+        # Attempt to create a new user
+        @user = User.new(
+          username: auth_hash["info"]["name"],
+          email: auth_hash["info"]["email"],
+          uid: auth_hash["uid"],
+          provider: auth_hash["provider"]
+        )
+        if @user.save
+          session[:user_id] = @user.id
+          flash[:success] = "Loged in successfully"
+          redirect_to root_path
+        else
+          flash[:error] = "Could not tog in"
+          redirect_to root_path
+      else
+        session[:user_id] = @user.id
+        flash[:success] = "Logged in successfully"
+        redirect_to root_path
+      end
+    else
+      flash[:error] = "Could not log in"
+      redirect_to root_path
+    end
+  end
+
   def login
     username = params[:username]
     if username and user = User.find_by(username: username)
