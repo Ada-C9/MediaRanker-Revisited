b9d1f8396036f0dfa0920d87082395d2533a6085
Zheng Cao <chori84@gmail.com>
2018-04-16 19:53:20 -0700

    Finish SessionsController test for login_form action

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,5 +1,19 @@
 require "test_helper"
 
 describe SessionsController do
+  describe "login_form" do
+    it "succeeds" do
+      get login_path
 
+      must_respond_with :success
+    end
+  end
+
+  describe "login" do
+
+  end
+
+  describe "logout" do
+
+  end
 end
