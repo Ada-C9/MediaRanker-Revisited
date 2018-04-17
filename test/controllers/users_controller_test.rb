3d9703b3cf13af96477d741bd1e3b613495a62ec
LetiTran <leticiastran@gmail.com>
2018-04-17 15:43:19 -0700

    Added tests for users_controller#index

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,5 +1,34 @@
 require 'test_helper'
 
 describe UsersController do
+  describe 'Index' do
+    it "succeeds when there are users" do
+      #  Act
+      get users_path
+      # Assert
+      must_respond_with :success
+    end
 
+    it "succeeds when there are no users" do
+      # Arrange
+      Work.destroy_all
+      User.destroy_all
+      assert User.all.empty?
+
+      # Act
+      get users_path
+
+      #  Assert
+      must_respond_with :success
+    end
+  end
+
+  describe 'Show' do
+    it "renders 404 not_found for a wrong user ID" do
+      get work_path("wrong id")
+
+      must_respond_with :missing
+    end
+
+  end
 end
