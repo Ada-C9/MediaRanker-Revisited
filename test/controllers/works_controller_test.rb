4fa525023cee7a7b73df2f9d833ad5a91cc056c4
Nora Peters <norapeters5@gmail.com>
2018-04-16 17:31:52 -0700

    adds works_controller tests for root, index, new, create, and show

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,19 +1,32 @@
 require 'test_helper'
+require 'pry'
 
 describe WorksController do
   describe "root" do
     it "succeeds with all media types" do
       # Precondition: there is at least one media of each category
-
+      get root_path
+      must_respond_with :success
     end
 
     it "succeeds with one media type absent" do
       # Precondition: there is at least one media in two of the categories
+      work = works(:movie)
+      work.delete
 
+      get root_path
+      must_respond_with :success
     end
 
     it "succeeds with no media" do
+      works(:album).destroy
+      works(:another_album).destroy
+      works(:poodr).destroy
+      works(:movie).destroy
 
+      Work.all.count.must_equal 0
+      get root_path
+      must_respond_with :success
     end
   end
 
@@ -22,52 +35,89 @@ describe WorksController do
 
   describe "index" do
     it "succeeds when there are works" do
-
+      get works_path
+      must_respond_with :success
     end
 
     it "succeeds when there are no works" do
+      works(:album).destroy
+      works(:another_album).destroy
+      works(:poodr).destroy
+      works(:movie).destroy
 
+      Work.all.count.must_equal 0
+      get works_path
+      must_respond_with :success
     end
   end
 
   describe "new" do
     it "succeeds" do
-
+      get new_work_path
+      must_respond_with :success
     end
   end
 
   describe "create" do
     it "creates a work with valid data for a real category" do
+      proc {
+        post works_path, params: {
+          work: {
+            category: "movie",
+            title: "Superbad"
+          }
+        }
+      }.must_change 'Work.count', 1
 
+      must_respond_with :redirect
+      must_redirect_to work_path(Work.last)
     end
 
     it "renders bad_request and does not update the DB for bogus data" do
-
+      proc {
+        post works_path, params: {
+          work: {
+            category: "album",
+          }
+        }
+      }.must_change 'Work.count', 0
+      must_respond_with :bad_request
     end
 
     it "renders 400 bad_request for bogus categories" do
-
+      post works_path, params: {
+        work: {
+          category: "play",
+          title: "Hamlet"
+        }
+      }
+      must_respond_with :bad_request
     end
-
   end
 
   describe "show" do
     it "succeeds for an extant work ID" do
-
+      get work_path(works(:poodr).id)
+      must_respond_with :success
     end
 
     it "renders 404 not_found for a bogus work ID" do
-
+      works(:album).id = 'testid'
+      get work_path(works(:album).id)
+      must_respond_with :not_found
     end
   end
 
   describe "edit" do
     it "succeeds for an extant work ID" do
-
+      get edit_work_path(works(:movie).id)
+      must_respond_with :success
     end
 
     it "renders 404 not_found for a bogus work ID" do
-
+      works(:poodr).id = 239847
+      get edit_work_path(works(:poodr).id)
+      must_respond_with :not_found
     end
   end
 
