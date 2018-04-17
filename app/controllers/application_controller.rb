396ebc602bc5ef39ff81e13ddaf5c6030380a64d
Lily Sky <lilyxsky@gmail.com>
2018-04-17 16:34:13 -0700

    controller filters for user authorization added; wave 3 code completed; refactoring + testing needs attention"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   before_action :find_user
+  before_action :require_login, except: [:root]
+
 
   def render_404
     # DPR: this will actually render a 404 page in production
     raise ActionController::RoutingError.new('Not Found')
+  end
+
+  def require_login
+    if @login_user.nil?
+      flash[:status] = :error
+      flash[:result_text] = "You must be logged in to view this section"
+      redirect_to root_path
+    end
   end
 
 private
@@ -14,4 +23,6 @@ private
       @login_user = User.find_by(id: session[:user_id])
     end
   end
+
+
 end
