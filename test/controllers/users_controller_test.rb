37d7ef2abbfe64a6769195ac471740b147fbae0e
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-04-16 17:12:53 -0700

    created root and index tests for works

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,5 +1,14 @@
 require 'test_helper'
 
 describe UsersController do
+	it "should get index" do
+    get users_path
+		must_respond_with :success
+  end
+
+	# def show
+	# 	@user = User.find_by(id: params[:id])
+	# 	render_404 unless @user
+	# end
 
 end
--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -3,17 +3,23 @@ require 'test_helper'
 describe WorksController do
   describe "root" do
     it "succeeds with all media types" do
-      # Precondition: there is at least one media of each category
-
+      Work.count.must_equal 4
+      get root_path
+      must_respond_with :success
     end
 
     it "succeeds with one media type absent" do
-      # Precondition: there is at least one media in two of the categories
-
+      works(:movie).destroy
+      Work.count.must_equal 3 # not needed but just to confirm that is worked
+      get root_path
+      must_respond_with :success
     end
 
     it "succeeds with no media" do
-
+      Work.all.each { |work| work.destroy }
+      Work.count.must_equal 0 # not needed but just to confirm that is worked
+      get root_path
+      must_respond_with :success
     end
   end
 
@@ -22,11 +28,15 @@ describe WorksController do
 
   describe "index" do
     it "succeeds when there are works" do
-
+      get works_path
+      must_respond_with :success
     end
 
     it "succeeds when there are no works" do
-
+      Work.all.each { |work| work.destroy }
+      Work.count.must_equal 0 # not needed but just to confirm that is worked
+        get works_path
+      must_respond_with :success
     end
   end
 
@@ -106,7 +116,16 @@ describe WorksController do
     end
 
     it "succeeds for a logged-in user and a fresh user-vote pair" do
+      proc {
+        post login_path, params: { username: users(:kari).username }
+        voted_on_work = Work.find_by(id: works(:movie).id)
+        post upvote_path(voted_on_work.id), params: {
+          vote: { user: users(:kari), work: voted_on_work }
+        }
+      }.must_change 'Vote.count', 1
 
+      must_redirect_to work_path(works(:movie))
+      must_respond_with :redirect
     end
 
     it "redirects to the work page if the user has already voted for that work" do
