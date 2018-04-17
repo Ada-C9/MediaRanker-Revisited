6c6d51f0d6191525624edce411d82e4ed35b0d57
Lily Sky <lilyxsky@gmail.com>
2018-04-17 13:32:24 -0700

    log in exclusively with GH works

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,26 +3,56 @@ class SessionsController < ApplicationController
   end
 
   def login
-    username = params[:username]
-    if username and user = User.find_by(username: username)
-      session[:user_id] = user.id
-      flash[:status] = :success
-      flash[:result_text] = "Successfully logged in as existing user #{user.username}"
-    else
-      user = User.new(username: username)
-      if user.save
-        session[:user_id] = user.id
-        flash[:status] = :success
-        flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
+    auth_hash = request.env['omniauth.auth']
+
+    if auth_hash[:uid]
+      @user = User.find_by(uid: auth_hash[:uid], provider: "github")
+
+      if @user.nil?
+        @user = User.build_from_github(auth_hash)
+        successful_save = @user.save
+
+        if successful_save
+          flash[:success] = "Successfully logged in as existing user #{@user.username}"
+          session[:user_id] = @user.id
+          redirect_to root_path
+        else
+          flash[:error] = "An error occured in user creation"
+          redirect_to root_path
+        end
       else
-        flash.now[:status] = :failure
-        flash.now[:result_text] = "Could not log in"
-        flash.now[:messages] = user.errors.messages
-        render "login_form", status: :bad_request
-        return
+        flash[:status] = :success
+        flash[:result_text] = "Successfully logged in as existing user #{@user.username}"
+        session[:user_id] = @user.id
+        redirect_to root_path
       end
+
+
+    else
+      flash[:error] = "logging in through Github not successful"
+      redirect_to root_path
     end
-    redirect_to root_path
+
+    # username = params[:username]
+    # if username and user = User.find_by(username: username)
+    #   session[:user_id] = user.id
+    #   flash[:status] = :success
+    #   flash[:result_text] = "Successfully logged in as existing user #{user.username}"
+    # else
+    #   user = User.new(username: username)
+    #   if user.save
+    #     session[:user_id] = user.id
+    #     flash[:status] = :success
+    #     flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
+    #   else
+    #     flash.now[:status] = :failure
+    #     flash.now[:result_text] = "Could not log in"
+    #     flash.now[:messages] = user.errors.messages
+    #     render "login_form", status: :bad_request
+    #     return
+    #   end
+    # end
+    # redirect_to root_path
   end
 
   def logout
