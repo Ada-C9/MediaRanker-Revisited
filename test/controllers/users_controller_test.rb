1be5344262c5a44a5918a69e5db7d09a8f06eb03
Nora Peters <norapeters5@gmail.com>
2018-04-17 15:47:33 -0700

    User controller tests added and passing

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,4 +2,33 @@ require 'test_helper'
 
 describe UsersController do
 
+  describe "index" do
+    it "succeeds when there are users" do
+      get users_path
+      must_respond_with :success
+      User.all.count.must_equal 2
+    end
+
+    it "succeeds when there are no users" do
+      users(:dan).destroy
+      users(:kari).destroy
+
+      User.all.count.must_equal 0
+      get users_path
+      must_respond_with :success
+    end
+  end
+
+  describe "show" do
+    it "succeeds for an extant user ID" do
+      get user_path(users(:dan).id)
+      must_respond_with :success
+    end
+
+    it "renders 404 not_found for a bogus user ID" do
+      users(:kari).id = 'testid'
+      get user_path(users(:kari).id)
+      must_respond_with :not_found
+    end
+  end
 end
