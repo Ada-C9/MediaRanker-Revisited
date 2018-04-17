b21f45c6964b7ae17fd947aeddef49f889197366
LetiTran <leticiastran@gmail.com>
2018-04-16 20:53:30 -0700

    Added tests for works_controller#create

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -47,100 +47,119 @@ describe WorksController do
 
   describe "new" do
     it "succeeds" do
+      get new_work_path
+      must_respond_with :success
 
     end
   end
 
   describe "create" do
     it "creates a work with valid data for a real category" do
-      # it 'creates a user with valid data' do
-      #   # Act
-      #   proc { #proc block
-      #     post user_path(), params: {
-      #       name: {
-      #         name: "New User"
-      #       }  #Needs nesting because we are using forms, so it automatically returns the params in a nested form
-      #     }
-      #   }.must_change 'User.count', 1 #must change with a difference with 1
-      #
-      #
-      #   # Assert
-      #   must_respond_with :redirect
-      #   must_redirect_to authors_path
-      # end
+
+      proc {
+        post works_path, params: {
+          work: {
+            title: "New Work",
+            category:  "album"
+          }
+          }
+        }.must_change 'Work.count', 1
+
+        # Assert
+        must_respond_with :redirect
+        must_redirect_to work_path(Work.last.id)
+
+
+      end
+
+      it "renders not_found and does not update the DB for bogus data" do
+        proc {
+             post works_path, params: {
+               work: {
+                 title: "New work"
+                 # missing category
+               }
+             }
+           }.must_change 'Work.count', 0
+
+           must_respond_with :error
+      end
+
+      it "renders 400 bad_request for bogus categories" do
+        proc {
+             post works_path, params: {
+               work: {
+                 title: "New work",
+                category:  "wrong category"
+               }
+             }
+           }.must_change 'Work.count', 0
+
+           must_respond_with 400
+      end
 
     end
 
-    it "renders not_found and does not update the DB for bogus data" do
+    describe "show" do
+      it "succeeds for an extant work ID" do
 
+      end
+
+      it "renders 404 not_found for a bogus work ID" do
+
+      end
     end
 
-    it "renders 400 bad_request for bogus categories" do
+    describe "edit" do
+      it "succeeds for an extant work ID" do
 
+      end
+
+      it "renders 404 not_found for a bogus work ID" do
+
+      end
     end
 
+    describe "update" do
+      it "succeeds for valid data and an extant work ID" do
+
+      end
+
+      it "renders bad_request for bogus data" do
+
+      end
+
+      it "renders 404 not_found for a bogus work ID" do
+
+      end
+    end
+
+    describe "destroy" do
+      it "succeeds for an extant work ID" do
+
+      end
+
+      it "renders 404 not_found and does not update the DB for a bogus work ID" do
+
+      end
+    end
+
+    describe "upvote" do
+
+      it "redirects to the work page if no user is logged in" do
+
+      end
+
+      it "redirects to the work page after the user has logged out" do
+
+      end
+
+      it "succeeds for a logged-in user and a fresh user-vote pair" do
+
+      end
+
+      it "redirects to the work page if the user has already voted for that work" do
+
+      end
+    end
   end
-
-  describe "show" do
-    it "succeeds for an extant work ID" do
-
-    end
-
-    it "renders 404 not_found for a bogus work ID" do
-
-    end
-  end
-
-  describe "edit" do
-    it "succeeds for an extant work ID" do
-
-    end
-
-    it "renders 404 not_found for a bogus work ID" do
-
-    end
-  end
-
-  describe "update" do
-    it "succeeds for valid data and an extant work ID" do
-
-    end
-
-    it "renders bad_request for bogus data" do
-
-    end
-
-    it "renders 404 not_found for a bogus work ID" do
-
-    end
-  end
-
-  describe "destroy" do
-    it "succeeds for an extant work ID" do
-
-    end
-
-    it "renders 404 not_found and does not update the DB for a bogus work ID" do
-
-    end
-  end
-
-  describe "upvote" do
-
-    it "redirects to the work page if no user is logged in" do
-
-    end
-
-    it "redirects to the work page after the user has logged out" do
-
-    end
-
-    it "succeeds for a logged-in user and a fresh user-vote pair" do
-
-    end
-
-    it "redirects to the work page if the user has already voted for that work" do
-
-    end
-  end
-end
