d7e351150c03cc1830c4dfb646eae3ff3a422edc
Kat <kat.eastman@gmail.com>
2018-04-17 11:46:44 -0700

    omniauth changes for github user auth.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'works#root'
   get '/login', to: 'sessions#login_form', as: 'login'
-  post '/login', to: 'sessions#login'
+  get "/auth/:provider/callback", to: "sessions#login"
+  # post '/login', to: 'sessions#login'
   post '/logout', to: 'sessions#logout', as: 'logout'
 
   resources :works
