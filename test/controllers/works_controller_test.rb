fb1b0dc590b4ada9cd6d14537055da06ef46f9e9
Zheng Cao <chori84@gmail.com>
2018-04-16 19:42:55 -0700

    Finish WorksController upvote tests

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -186,20 +186,39 @@ describe WorksController do
   describe "upvote" do
 
     it "redirects to the work page if no user is logged in" do
-      
+      @login_user = nil
 
+      post upvote_path(works(:album).id)
+
+      must_respond_with :redirect
+      must_redirect_to work_path(works(:album).id)
     end
 
     it "redirects to the work page after the user has logged out" do
+      @login_user = nil
 
+      post upvote_path(works(:album).id)
+
+      must_respond_with :redirect
+      must_redirect_to work_path(works(:album).id)
     end
 
     it "succeeds for a logged-in user and a fresh user-vote pair" do
+      @login_user = users(:dan)
 
+      post upvote_path(works(:poodr).id)
+
+      must_respond_with :redirect
+      must_redirect_to work_path(works(:poodr).id)
     end
 
     it "redirects to the work page if the user has already voted for that work" do
+      @login_user = users(:dan)
 
+      post upvote_path(works(:album).id)
+
+      must_respond_with :redirect
+      must_redirect_to work_path(works(:album).id)
     end
   end
 end
