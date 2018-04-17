835a468590fc6c4b012df50b2cd410bc990d32c9
Hannah Cameron <hannahiscoding@gmail.com>
2018-04-17 12:42:28 -0700

    continuation of adding OmniAuth - changed users model, added amniauth to initialization, built create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,34 @@ class SessionsController < ApplicationController
   def login_form
   end
 
+  def create
+      auth_hash = request.env['omniauth.auth']
+      if auth_hash['uid']
+        @user = User.find_by(uid: auth_hash[:uid], provider: 'github')
+        if @user.nil?
+          @user = User.new(
+          name: auth_hash['info']['name'],
+          email: auth_hash['info']['email'],
+          uid: auth_hash[:uid],
+          provider: auth_hash[:provider])
+          if @user.save
+            session[:user_id] = @user.id
+            flash[:success] = "Logged in successfully"
+            redirect_to root_path
+          else
+            flash[:error] = "Could not log in"
+            redirect_to root_path
+        else
+          session[:user_id] = @user.id
+          flash[:success] = "Logged in successfully"
+          redirect_to root_path
+        end
+      else
+          flash[:error] = "Could not log in"
+          redirect_to root_path
+      end
+    end
+
   def login
     username = params[:username]
     if username and user = User.find_by(username: username)
