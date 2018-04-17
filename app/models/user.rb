800f7285f696f41916988cf62a8b721528e18bf3
Kat <kat.eastman@gmail.com>
2018-04-17 12:06:11 -0700

    added user#build_from_github and controller logic.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   has_many :ranked_works, through: :votes, source: :work
 
   validates :username, uniqueness: true, presence: true
+
+def build_from_github(auth_hash)
+user_data = {
+  uid: auth_hash[:uid],
+  name: auth_hash["info"]["name"],
+  email: auth_hash["info"]["email"],
+  provider: auth_hash[:provider]
+}
+
+  user = self.New(user_data)
+  return user if user.save
+end
+
 end
