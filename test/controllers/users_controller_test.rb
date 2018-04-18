0c45db93ccbb6cfc0538ebc1a3c83609a1f68629
Kiera <kiera.thomasson@gmail.com>
2018-04-17 17:02:29 -0700

    Added skeleton user tests.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,10 +2,22 @@ require 'test_helper'
 
 describe UsersController do
   describe 'index' do
+    it "succeeds when there are users" do
 
+    end
+
+    it "succeeds when there are no users" do
+
+    end
   end
 
   describe 'show' do
+    it "succeeds for an existing user" do
 
+    end
+
+    it "renders 404 not_found for a nonexistent user" do
+
+    end
   end
 end
