cb2bcf3302c4e73f535bd544de33bd138094ddb6
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-04-18 09:01:36 -0700

    Add login and migration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :find_user
+  # before_action :require_login
+  #
+  # def current_user
+  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  # end
+  #
+  # def require_login
+  #   if current_user.nil?
+  #     flash[:error] = "You must be logged in to view this section"
+  #     redirect_to session_path
+  #   end
+  # end
 
   def render_404
     # DPR: this will actually render a 404 page in production
