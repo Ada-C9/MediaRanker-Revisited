a115b8144235abff305f5bea51ba25ab17070b4f
Phoebe <phoebe.manning8@gmail.com>
2018-04-17 17:11:55 -0700

    refactors routes and application html to account for github login

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   get "/auth/github", as: 'github_login'
 
   root 'works#root'
-  get '/login', to: 'sessions#login_form', as: 'login'
-  post '/login', to: 'sessions#login'
+  # get '/login', to: 'sessions#login_form', as: 'login'
+  get '/login', to: 'sessions#login', as: 'login'
   post '/logout', to: 'sessions#logout', as: 'logout'
 
   resources :works
