3602b40bb7a1ca26469aae0c4f282764112b16cb
Kiera <kiera.thomasson@gmail.com>
2018-04-17 20:50:51 -0700

    Added index and show tests for user.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,21 +3,43 @@ require 'test_helper'
 describe UsersController do
   describe 'index' do
     it "succeeds when there are users" do
+      # Assumptions
+      User.count.must_be :>,0
 
+      # Act
+      get users_path
+
+      # Assert
+      must_respond_with :success
     end
 
-    it "succeeds when there are no users" do
 
+    it "succeeds when there are no users" do
+      skip
+      # Assumptions
+      # User.destroy_all
+      #
+      # # Act
+      # get user_path
+      #
+      # # Assert
+      # must_respond_with :success
     end
   end
 
   describe 'show' do
     it "succeeds for an existing user" do
+      get user_path(User.first)
 
+      must_respond_with :success
     end
 
     it "renders 404 not_found for a nonexistent user" do
+      user_id = User.last.id + 1
 
+      get user_path(user_id)
+
+      must_respond_with :not_found
     end
   end
 end
