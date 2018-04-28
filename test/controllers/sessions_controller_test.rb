52bc72b02a797f5aa09cd6a5c86cdc4ed7068e48
SelamawitA <Selamawit.ainalem@gmail.com>
2018-04-28 15:51:51 -0700

    set up tests for sesssion controller.

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,5 +1,26 @@
 require "test_helper"
 
 describe SessionsController do
+  describe 'Login' do
+    it 'will allow an existing user to log back in and recall their username' do
+      
+    end
 
+    it 'will create an account for a new valid user' do
+    end
+
+    it 'will render a bad_requst for a user who attempts to create an account with invalid data' do
+    end
+  end
+
+  describe 'Logout' do
+    it 'will destroy an existing account' do
+    end
+  end
+
+  describe 'Login Form' do
+    it 'success' do
+
+    end
+  end
 end
