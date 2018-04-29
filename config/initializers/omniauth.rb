1138da1461c96c30f162bcc96659e4c44654c224
SelamawitA <Selamawit.ainalem@gmail.com>
2018-04-28 22:31:37 -0700

    adding initializer edit

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,10 @@
-Rails.application.config.middleware.use
-  OmniAuth::Builder do
-    provider :github,
-    ENV["GITHUB_CLIENT_ID"],
-    ENV["GITHUB_CLIENT_SECRET"], scope: "user:email"
-  end
+# Rails.application.config.middleware.use
+#   OmniAuth::Builder do
+#     provider :github,
+#     ENV["GITHUB_CLIENT_ID"],
+#     ENV["GITHUB_CLIENT_SECRET"], scope: "user:email"
+#   end
+#
+# # ad16c61430c05735f0195e4f95ec756e1f83607e
+# #
+# # b92e407af7caf19b57a9886ea1e4b02b036e05f3
