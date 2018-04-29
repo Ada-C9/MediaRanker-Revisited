cf7f899ecc5a54d12b9661e4fb2bf9bac40bddce
Phoebe <phoebe.manning8@gmail.com>
2018-04-29 12:52:36 -0700

    fixes issue with login controller filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,23 @@ class ApplicationController < ActionController::Base
 
   before_action :find_user
 
+  before_action :require_login
+
+  def current_user
+    # @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    if session[:user_id]
+      @current_user = User.find(session[:user_id])
+    end
+  end
+  #
+  def require_login
+    if current_user.nil?
+      flash[:status] = :success
+      flash[:result_text] = "You must be logged in to view this section"
+      redirect_to root_path
+    end
+  end
+
   def render_404
     # DPR: this will actually render a 404 page in production
     raise ActionController::RoutingError.new('Not Found')
