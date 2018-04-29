d5a53ee282679362b938b4df747c7d9acd73410c
Kaitlin Forsman <kcforsman@outlook.com>
2018-04-28 23:27:40 -0700

    adjusted user controller tests to take into account new permissions for viewing website through OAuth

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,35 +2,42 @@ require 'test_helper'
 
 describe UsersController do
   describe 'index' do
-    it 'succeeds when there are users' do
+    it 'succeeds when there are users and user is logged in' do
+      perform_login(users(:dan))
+
       get users_path
 
       must_respond_with :success
     end
-    it 'succeeds when there are no users' do
-      User.all.each do |user|
-        if !user.votes.empty?
-          user.votes.each { |vote| vote.destroy }
-        end
-        user.destroy
-      end
-
+    it 'redirects to root if there is no logged in user' do
       get users_path
 
-      must_respond_with :success
+      must_respond_with :redirect
+      must_redirect_to root_path
     end
   end
   describe 'show' do
-    it 'succeeds for an extant work ID' do
-      get user_path(users(:dan).id)
+    it 'succeeds for an extant work ID and user is logged in' do
+      perform_login(users(:dan))
+      user = users(:kari)
+
+      get user_path(user.id)
 
       must_respond_with :success
     end
+    it 'redirects to root if no one is logged in' do
+      get user_path(users(:dan).id)
+
+      must_respond_with :redirect
+      must_redirect_to root_path
+    end
     it 'renders 404 not_found for an invalid work ID' do
-      id = users(:dan).id
-      byebye = User.find_by(id: id)
-      byebye.votes.each {|vote| vote.destroy }
-      byebye.destroy
+      user = users(:dan)
+      id = user.id
+      user.votes.each {|vote| vote.destroy }
+      user.destroy
+
+      perform_login(users(:kari))
 
       get user_path(id)
 
