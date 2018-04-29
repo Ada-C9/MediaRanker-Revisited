1be5632b252b44649950bdcd8ba24e2172e555aa
Anne <annehwatson@gmail.com>
2018-04-28 17:40:56 -0700

    added setup block to tets helper

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,5 +1,13 @@
 require "test_helper"
 
 describe SessionsController do
+  describe "auth_callback" do
+    it "logs in an existing user" do
 
+    end
+
+    it "creates a new user if one does not already exist" do
+
+    end
+  end
 end
