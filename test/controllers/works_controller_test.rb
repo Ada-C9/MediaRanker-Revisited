f87781e40fe29d93378acd7b899352b4c2340127
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-04-29 14:16:41 -0700

    Tests for works_controllers done

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -11,7 +11,6 @@ describe WorksController do
       end
       get root_path
       must_respond_with :success
-
     end
 
     it "succeeds with one media type absent" do
@@ -19,14 +18,12 @@ describe WorksController do
       Work.by_category("album").destroy_all
       get root_path
       must_respond_with :success
-
     end
 
     it "succeeds with no media" do
       Work.destroy_all
       get root_path
       must_respond_with :success
-
     end
   end
 
@@ -38,28 +35,23 @@ describe WorksController do
       Work.count.must_be :>, 0
       get works_path
       must_respond_with :success
-
     end
 
     it "succeeds when there are no works" do
       Work.destroy_all
       get works_path
       must_respond_with :success
-
     end
   end
 
   describe "new" do
-
     it "succeeds" do
       get new_work_path
       must_respond_with :success
-
     end
   end
 
   describe "create" do
-
     it "creates a work with valid data for a real category" do
       proc {
         post works_path, params: {
@@ -71,7 +63,6 @@ describe WorksController do
 
       must_respond_with :redirect
       must_redirect_to work_path(Work.last)
-
     end
 
     it "renders bad_request and does not update the DB for bogus data" do
@@ -84,7 +75,6 @@ describe WorksController do
       }.wont_change 'Work.count'
 
       must_respond_with :bad_request
-
     end
 
     it "renders 400 bad_request for bogus categories" do
@@ -99,14 +89,11 @@ describe WorksController do
         }.wont_change 'Work.count'
         must_respond_with :bad_request
       end
-
     end
   end
 
-
-
   describe "show" do
-    it "succeeds for an extant work ID" do
+    it "succeeds for an existant work ID" do
       get work_path(works(:album).id)
       must_respond_with :success
     end
@@ -121,7 +108,6 @@ describe WorksController do
     it "succeeds for an extant work ID" do
       get edit_work_path(works(:album).id)
       must_respond_with :success
-
     end
 
     it "renders 404 not_found for a bogus work ID" do
@@ -131,22 +117,19 @@ describe WorksController do
   end
 
   describe "update" do
-    it "succeeds for valid data and an extant work ID" do
+    it "succeeds for valid data and an existant work ID" do
       get work_path(works(:album).id)
       must_respond_with :success
-
     end
 
     it "renders bad_request for bogus data" do
       get work_path("foo")
       must_respond_with 404
-
     end
 
     it "renders 404 not_found for a bogus work ID" do
       get work_path("foo")
       must_respond_with 404
-
     end
   end
 
@@ -162,7 +145,6 @@ describe WorksController do
 
       must_respond_with :redirect
       must_redirect_to root_path
-
     end
 
     it "renders 404 not_found and does not update the DB for a bogus work ID" do
@@ -171,22 +153,40 @@ describe WorksController do
     end
   end
 
-  # describe "upvote" do
-  #
-  #   it "redirects to the work page if no user is logged in" do
-  #
-  #   end
-  #
-  #   it "redirects to the work page after the user has logged out" do
-  #
-  #   end
-  #
-  #   it "succeeds for a logged-in user and a fresh user-vote pair" do
-  #
-  #   end
-  #
-  #   it "redirects to the work page if the user has already voted for that work" do
-  #
-  #   end
-  # end
+  describe "upvote" do
+
+    it "redirects to the work page if no user is logged in" do
+      @login_user = nil
+
+      post upvote_path(works(:album).id)
+      must_respond_with :redirect
+      must_redirect_to work_path(works(:album).id)
+    end
+
+    it "redirects to the work page after the user has logged out" do
+
+      @login_user = nil
+
+      post upvote_path(works(:album).id)
+      must_respond_with :redirect
+      must_redirect_to work_path(works(:album).id)
+    end
+
+    it "succeeds for a logged-in user and a fresh user-vote pair" do
+      perform_login(users(:kari))
+
+      proc { post upvote_path(works(:poodr).id) }.must_change 'Vote.count', 1
+      must_respond_with :redirect
+      must_redirect_to work_path(works(:poodr).id)
+
+    end
+
+    it "redirects to the work page if the user has already voted for that work" do
+      perform_login(users(:kari))
+
+      proc { post upvote_path(works(:album).id) }.must_change 'Vote.count', 0
+      must_respond_with :redirect
+      must_redirect_to work_path(works(:album).id)
+    end
+  end
 end
