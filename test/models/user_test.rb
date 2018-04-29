710e48f0747398b0bc58a4fe57a0826f15d88622
Samantha Berk <sberk10@gmail.com>
2018-04-29 13:52:17 -0700

    Add uid and provider to make failing tests pass

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -28,12 +28,12 @@ describe User do
 
     it "requires a unique username" do
       username = "test username"
-      user1 = User.new(username: username)
+      user1 = User.new(username: username, email: 'email@gmail.com', uid: 123456789, provider: 'github')
 
       # This must go through, so we use create!
       user1.save!
 
-      user2 = User.new(username: username)
+      user2 = User.new(username: username, email: 'newemail@gmail.com', uid: 202020202, provider: 'github')
       result = user2.save
       result.must_equal false
       user2.errors.messages.must_include :username
