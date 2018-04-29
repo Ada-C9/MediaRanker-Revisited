11037402732e127dffd3551d29e13707a2bb32a3
SelamawitA <Selamawit.ainalem@gmail.com>
2018-04-29 13:18:04 -0700

    modified user model with three migrations (uid,email, and provider) to account for information coming from Git auth-hash. Also, modified log-in/log-out to utilize Oauth.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,43 +1,79 @@
 class SessionsController < ApplicationController
-  def login_form
-  end
 
-  def login
-    username = params[:username]
-    if username and user = User.find_by(username: username)
-      session[:user_id] = user.id
-      flash[:status] = :success
-      flash[:result_text] = "Successfully logged in as existing user #{user.username}"
-    else
-      user = User.new(username: username)
-      if user.save
-        session[:user_id] = user.id
-        flash[:status] = :success
-
-        flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
+  def create
+    auth_hash = request.env['omniauth.auth']
+    if auth_hash['uid']
+      @user = User.find_by(uid: auth_hash['uid'])
+      if @user.nil?
+        create_new_user
       else
-
-        flash.now[:status] = :failure
-        flash.now[:result_text] = "Could not log in"
-        flash.now[:messages] = user.errors.messages
-
-        render "login_form", status: :bad_request
-        return
+        flash[:status] = :success
+        flash[:result_text] = "Logged in successfully, welcome back #{@user.username}"
       end
+      session[:user_id] = @user.id
+    else
+      flash[:status] = :failure
+      flash[:result_text] = "Log in has failed"
     end
     redirect_to root_path
   end
 
-  def logout
+  def destroy
     session[:user_id] = nil
     flash[:status] = :success
-    flash[:result_text] = "Successfully logged out"
+    flash[:result_text] = "You've logged out"
     redirect_to root_path
   end
 
-
-  def create
-    auth_hash = request.env['omniauth.auth']
-    raise
+  private
+  def create_new_user
+      auth_hash = request.env['omniauth.auth']
+      new_user = User.new(username: auth_hash['info']['nickname'], email: auth_hash['info']['email'], uid: auth_hash['uid'], provider: 'Github')
+      # binding.pry
+      if new_user.save
+        flash[:status] = :success
+        flash[:result_text] = "Your account has been generated #{new_user.username}!"
+      else
+        flash[:status] = :failure
+        flash[:result_text] = "Something has gone wrong in the account generation process."
+      end
   end
+  # def logout
+  #   session[:user_id] = nil
+  #   flash[:status] = :success
+  #   flash[:result_text] = "Successfully logged out"
+  #   redirect_to root_path
+  # end
+
+  # def login_form
+  # end
+
+  # def login
+  #   username = params[:username]
+  #   if username and user = User.find_by(username: username)
+  #     session[:user_id] = user.id
+  #     flash[:status] = :success
+  #     flash[:result_text] = "Successfully logged in as existing user #{user.username}"
+  #   else
+  #     user = User.new(username: username)
+  #     if user.save
+  #       session[:user_id] = user.id
+  #       flash[:status] = :success
+  #
+  #       flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
+  #     else
+  #
+  #       flash.now[:status] = :failure
+  #       flash.now[:result_text] = "Could not log in"
+  #       flash.now[:messages] = user.errors.messages
+  #
+  #       render "login_form", status: :bad_request
+  #       return
+  #     end
+  #   end
+  #   redirect_to root_path
+  # end
+
+
+
 end
