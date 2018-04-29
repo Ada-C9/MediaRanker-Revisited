1ea9f16907d9a06a8c427b06e29cce00a36c2283
Anne <annehwatson@gmail.com>
2018-04-29 16:38:02 -0700

    log in test

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -3,7 +3,12 @@ require "test_helper"
 describe SessionsController do
   describe "auth_callback" do
     it "logs in an existing user" do
+      user = User.first
+      login(user)
 
+      must_respond_with :redirect
+      must_redirect_to root_path
+      session[:user_id].must_equal user.id
     end
 
     it "creates a new user if one does not already exist" do
