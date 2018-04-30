125d06417be401b7fc3d4234d15b76f5ece44e7e
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-04-30 15:57:05 -0700

    updated yml

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/auth/:provider/callback", to: "sessions#create"
+  get "/auth/:provider/callback", to: "sessions#create", as: 'login'
   get '/auth/github', as: 'github_login'
 
   root 'works#root'
