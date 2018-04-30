c6cfa5da70ea3f830e98b3ddf06fef919a309f05
Jackie <jonofre16@hotmail.com>
2018-04-29 20:15:01 -0700

    Added auth github and callback route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'works#root'
+  get '/auth/github', as: 'github_login'
+  get '/auth/github/callback', to: 'sessions#create', as: 'auth_callback'
+
   get '/login', to: 'sessions#login_form', as: 'login'
   post '/login', to: 'sessions#login'
   post '/logout', to: 'sessions#logout', as: 'logout'
