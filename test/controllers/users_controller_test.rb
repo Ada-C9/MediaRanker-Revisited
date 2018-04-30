48c96191ec337b2e34268b0c36ca49e49bc74599
Abiaina <abinnet.a@gmail.com>
2018-04-29 20:10:11 -0700

    user controller testing complete

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,5 +1,32 @@
 require 'test_helper'
 
 describe UsersController do
+  describe "index" do
+    it "succeeds with at least 1 user" do
+      User.all.nil?.must_equal false
+      get users_path
+      must_respond_with :success
+    end
 
+    it "succeeds with no users" do
+      Vote.destroy_all
+      User.destroy_all
+      User.count.must_equal 0
+      get users_path
+      must_respond_with :success
+    end
+  end
+
+  describe "show" do
+    it "succeeds with existing user" do
+      user = users(:kari)
+      get user_path(user.id)
+      must_respond_with :success
+    end
+
+    it "it renders missing page if non existing user id given" do
+      get user_path(-1)
+      must_respond_with :missing
+    end
+  end
 end
