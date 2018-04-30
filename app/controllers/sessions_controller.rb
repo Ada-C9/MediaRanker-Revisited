258ede0f24376e137a1781a9f3f25796ee675598
Aruna <aruna.thungathurthi@gmail.com>
2018-04-30 13:04:53 -0700

    all tests passing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,31 +1,44 @@
 class SessionsController < ApplicationController
-  def login_form
-  end
+  before_action :require_login, except: [:login]
 
   def login
     auth_hash = request.env['omniauth.auth']
 
-    if auth_hash['uid']
-      @user =  User.find_by(uid: auth_hash[:uid], provider: 'github')
+    if auth_hash[:uid]
+      @user = User.find_by(uid: auth_hash[:uid], provider: 'github')
+      # @user = User.find_by(uid: auth_hash[:uid], provider: params[:provider])
 
       if @user.nil?
         @user = User.build_from_github(auth_hash)
         successful_save = @user.save
+
         if successful_save
-          flash[:success] = "Logged in suucessfully"
           session[:user_id] = @user.id
+          flash[:status] = :success
+          flash[:result_text] = "Successfully created new user #{@user.username} with ID #{@user.id}"
           redirect_to root_path
         else
-          flash[:error] = "Some error happened in User creation"
-          redirect_to root_path
+          flash.now[:status] = :failure
+          flash.now[:result_text] = "Could not log in"
+          flash.now[:messages] = user.errors.messages
+          redirect_back fallback_location: auth_callback_path
         end
-      else
-        flash[:success] = "Logged in successfully"
-        session[:user_id] = @user.id
 
+      else
+        session[:user_id] = @user.id
+        flash[:status] = :success
+        flash[:result_text] = "Successfully logged in as existing user #{@user.username}"
         redirect_to root_path
       end
+
+    else
+      flash.now[:status] = :failure
+      flash.now[:result_text] = "Logging in through GitHub not successful"
+      redirect_back fallback_location: auth_callback_path
     end
+
+
+  end
     # username = params[:username]
     # if username and user = User.find_by(username: username)
     #   session[:user_id] = user.id
@@ -46,7 +59,7 @@ class SessionsController < ApplicationController
     #   end
     # end
     # redirect_to root_path
-  end
+  
 
   def logout
     session[:user_id] = nil
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,17 +3,17 @@ require 'test_helper'
 describe User do
   describe "relations" do
     it "has a list of votes" do
-      dan = users(:dan)
-      dan.must_respond_to :votes
-      dan.votes.each do |vote|
+      ada = users(:ada)
+      ada.must_respond_to :votes
+      ada.votes.each do |vote|
         vote.must_be_kind_of Vote
       end
     end
 
     it "has a list of ranked works" do
-      dan = users(:dan)
-      dan.must_respond_to :ranked_works
-      dan.ranked_works.each do |work|
+      ada = users(:ada)
+      ada.must_respond_to :ranked_works
+      ada.ranked_works.each do |work|
         work.must_be_kind_of Work
       end
     end
