07233077c4a76fa96260591f43249134c9979157
knockknockhusthere <angela.huxia@gmail.com>
2018-04-30 17:04:40 -0700

    got upvotes semi working in works controller

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -4,229 +4,245 @@ require 'pry'
 describe WorksController do
 
   describe 'logged in user' do
-    before do
-      login(User.first)
-    end
-  end
 
-  describe "root" do
-    it "succeeds with all media types" do
-      # Precondition: there is at least one media of each category
-      works = Work.where(category: "work")
-      movies = Work.where(category: "movie")
-      albums = Work.where(category: "album")
+    describe "root" do
+      it "succeeds with all media types" do
+        # Precondition: there is at least one media of each category
+login(User.first)
+        works = Work.where(category: "work")
+        movies = Work.where(category: "movie")
+        albums = Work.where(category: "album")
 
-      get root_path
-      must_respond_with :success
-    end
+        get root_path
+        must_respond_with :success
+      end
 
-    it "succeeds with one media type absent" do
-      # Precondition: there is at least one media in two of the categories
-      works = Work.where(category: "work")
-      albums = Work.where(category: "album")
+      it "succeeds with one media type absent" do
+        # Precondition: there is at least one media in two of the categories
+        login(User.first)
+        works = Work.where(category: "work")
+        albums = Work.where(category: "album")
 
-      get root_path
-      must_respond_with :success
-    end
+        get root_path
+        must_respond_with :success
+      end
 
-    it "succeeds with no media" do
-      get root_path
-      must_respond_with :success
-    end
-  end
-
-  CATEGORIES = %w(albums books movies)
-  INVALID_CATEGORIES = ["nope", "42", "", "  ", "albumstrailingtext"]
-
-  describe "index" do
-    it "succeeds when there are works" do
-      Work.count.must_be :>, 0
-            #act
-      get works_path
-            #assert
-      must_respond_with :success
+      it "succeeds with no media" do
+        login(User.first)
+        get root_path
+        must_respond_with :success
+      end
     end
 
-    it "succeeds when there are no works" do
-      Work.destroy_all
-      Work.all.length.must_equal 0
-      get works_path
+    CATEGORIES = %w(albums books movies)
+    INVALID_CATEGORIES = ["nope", "42", "", "  ", "albumstrailingtext"]
 
-      must_respond_with :success
-    end
-  end
+    describe "index" do
+      it "succeeds when there are works" do
+        login(User.first)
+        Work.count.must_be :>, 0
+        #act
+        get works_path
+        #assert
+        must_respond_with :success
+      end
 
-  describe "new" do
-    it "succeeds" do
-      get new_work_path
-      must_respond_with :success
-    end
-  end
+      it "succeeds when there are no works" do
+        login(User.first)
+        Work.destroy_all
+        Work.all.length.must_equal 0
+        get works_path
 
-  describe "create" do
-    it "creates a work with valid data for a real category" do
-      work_data = {
-        title: 'controller test work',
-        creator: "testtest",
-        publication_year: 2003,
-        category: "movie"
-      }
-      old_work_count = Work.count
-      #Assumptions
-      Work.new(work_data).must_be :valid?
-
-      #Act
-      post works_path, params: { work: work_data }
-
-      #assert
-      #check http response
-      must_redirect_to work_path(Work.last)
-      #check database
-      Work.count.must_equal old_work_count + 1
-      Work.last.title.must_equal work_data[:title]
+        must_respond_with :success
+      end
     end
 
-    it "renders bad_request and does not update the DB for bogus data" do
-      old_work_count = Work.count
-      work_data = {
-        title: nil,
-        category: "album"
-      }
-      Work.new(work_data).wont_be :valid?
-      #act
-      post works_path, params: {work: work_data}
-
-      must_respond_with :bad_request
-      Work.count.must_equal old_work_count
+    describe "new" do
+      it "succeeds" do
+        login(User.first)
+        get new_work_path
+        must_respond_with :success
+      end
     end
 
-    it "renders 400 bad_request for bogus categories" do
-      old_work_count = Work.count
-      work_data = {
-        title: nil,
-        category: "cotton candy"
-      }
-      Work.new(work_data).wont_be :valid?
-      #act
-      post works_path, params: {work: work_data}
+    describe "create" do
+      it "creates a work with valid data for a real category" do
+        login(User.first)
+        work_data = {
+          title: 'controller test work',
+          creator: "testtest",
+          publication_year: 2003,
+          category: "movie"
+        }
+        old_work_count = Work.count
+        #Assumptions
+        Work.new(work_data).must_be :valid?
 
-      must_respond_with :bad_request
-      Work.count.must_equal old_work_count
-    end
+        #Act
+        post works_path, params: { work: work_data }
 
-  end
+        #assert
+        #check http response
+        must_redirect_to work_path(Work.last)
+        #check database
+        Work.count.must_equal old_work_count + 1
+        Work.last.title.must_equal work_data[:title]
+      end
 
-  describe "show" do
-    it "succeeds for an extant work ID" do
-      get work_path(Work.first)
-      must_respond_with :success
-    end
+      it "renders bad_request and does not update the DB for bogus data" do
+        login(User.first)
+        old_work_count = Work.count
+        work_data = {
+          title: nil,
+          category: "album"
+        }
+        Work.new(work_data).wont_be :valid?
+        #act
+        post works_path, params: {work: work_data}
 
-    it "renders 404 not_found for a bogus work ID" do
-      work_id = Work.last.id + 1
+        must_respond_with :bad_request
+        Work.count.must_equal old_work_count
+      end
 
-      get work_path(work_id)
-      must_respond_with :not_found
-    end
-  end
+      it "renders 400 bad_request for bogus categories" do
+        login(User.first)
+        old_work_count = Work.count
+        work_data = {
+          title: nil,
+          category: "cotton candy"
+        }
+        Work.new(work_data).wont_be :valid?
+        #act
+        post works_path, params: {work: work_data}
 
-  describe "edit" do
-    it "succeeds for an extant work ID" do
-      get edit_work_path(Work.first)
-      must_respond_with :success
-    end
-
-    it "renders 404 not_found for a bogus work ID" do
-      get edit_work_path(Work.last.id + 1)
-      must_respond_with :not_found
-    end
-  end
-
-  describe "update" do
-    it "succeeds for valid data and an extant work ID" do
-      work = Work.first
-      work_data = work.attributes
-      work_data[:title] = "some updated title"
-
-      work.assign_attributes(work_data)
-      work.must_be :valid?
-
-      patch work_path(work), params: {work: work_data}
-
-      must_redirect_to work_path(work)
-      work.reload
-      work.title.must_equal work_data[:title]
+        must_respond_with :bad_request
+        Work.count.must_equal old_work_count
+      end
 
     end
 
-    it "renders not_found for bogus data" do
-      work = Work.first
-      work_data = work.attributes
-      work_data[:title] = ""
+    describe "show" do
+      it "succeeds for an extant work ID" do
+        login(User.first)
+        get work_path(Work.first)
+        must_respond_with :success
+      end
 
-      work.assign_attributes(work_data)
-      work.wont_be :valid?
+      it "renders 404 not_found for a bogus work ID" do
+        login(User.first)
+        work_id = Work.last.id + 1
 
-      patch work_path(work.id), params: {work: work_data}
-
-      must_respond_with :not_found
-      work.reload
-      work.title.wont_equal work_data[:title]
+        get work_path(work_id)
+        must_respond_with :not_found
+      end
     end
 
-    it "renders 404 not_found for a bogus work ID" do
-      work_id = Work.last.id + 1
+    describe "edit" do
+      it "succeeds for an extant work ID" do
+        login(User.first)
+        get edit_work_path(Work.first)
+        must_respond_with :success
+      end
 
-      patch work_path(work_id)
-
-      must_respond_with :not_found
-    end
-  end
-
-  describe "destroy" do
-    it "succeeds for an extant work ID" do
-      work_id = Work.first.id
-      old_count = Work.count
-      delete work_path(work_id)
-      # must_respond_with :success
-
-      must_redirect_to root_path
-      Work.count.must_equal old_count - 1
-
-      Work.find_by(id: work_id).must_be_nil
+      it "renders 404 not_found for a bogus work ID" do
+        login(User.first)
+        get edit_work_path(Work.last.id + 1)
+        must_respond_with :not_found
+      end
     end
 
-    it "renders 404 not_found and does not update the DB for a bogus work ID" do
-      work_id = Work.last.id + 1
+    describe "update" do
+      it "succeeds for valid data and an extant work ID" do
+        login(User.first)
+        work = Work.first
+        work_data = work.attributes
+        work_data[:title] = "some updated title"
 
-      patch work_path(work_id)
+        work.assign_attributes(work_data)
+        work.must_be :valid?
 
-      must_respond_with :not_found
+        patch work_path(work), params: {work: work_data}
 
+        must_redirect_to work_path(work)
+        work.reload
+        work.title.must_equal work_data[:title]
+
+      end
+
+      it "renders not_found for bogus data" do
+        login(User.first)
+        work = Work.first
+        work_data = work.attributes
+        work_data[:title] = ""
+
+        work.assign_attributes(work_data)
+        work.wont_be :valid?
+
+        patch work_path(work.id), params: {work: work_data}
+
+        must_respond_with :not_found
+        work.reload
+        work.title.wont_equal work_data[:title]
+      end
+
+      it "renders 404 not_found for a bogus work ID" do
+        login(User.first)
+        work_id = Work.last.id + 1
+
+        patch work_path(work_id)
+
+        must_respond_with :not_found
+      end
     end
-  end
 
-  describe "upvote" do
+    describe "destroy" do
+      it "succeeds for an extant work ID" do
+        login(User.first)
+        work_id = Work.first.id
+        old_count = Work.count
+        delete work_path(work_id)
+        # must_respond_with :success
 
-    it "redirects to the work page if no user is logged in" do
-      work = Work.first
-      vote_count = work.vote_count
+        must_redirect_to root_path
+        Work.count.must_equal old_count - 1
 
-      post upvote_path(work.id)
+        Work.find_by(id: work_id).must_be_nil
+      end
 
-      must_redirect_to work_path(work)
+      it "renders 404 not_found and does not update the DB for a bogus work ID" do
+        login(User.first)
+        work_id = Work.last.id + 1
+
+        patch work_path(work_id)
+
+        must_respond_with :not_found
+
+      end
     end
 
-    it "redirects to the work page after the user has logged out" do
+    describe "upvote" do
 
-    end
+      it "redirects to the work page if no user is logged in" do
+        work = Work.first
+        vote_count = work.vote_count
 
-    it "succeeds for a logged-in user and a fresh user-vote pair" do
+        post upvote_path(work.id)
+        must_respond_with :unauthorized
+        # must_redirect_to work_path(work)
 
-    end
+      end
 
-    it "redirects to the work page if the user has already voted for that work" do
+      it "redirects to the work page after the user has logged out" do
 
+      end
+
+      it "succeeds for a logged-in user and a fresh user-vote pair" do
+
+      end
+
+      it "redirects to the work page if the user has already voted for that work" do
+
+      end
     end
   end
 
@@ -280,11 +296,10 @@ describe WorksController do
       old_count = Work.count
       delete work_path(work_id)
       # must_respond_with :success
-
-      must_redirect_to root_path
+      #
+      # must_redirect_to root_path
       must_respond_with :unauthorized
       Work.count.must_equal old_count
     end
   end
-
 end
