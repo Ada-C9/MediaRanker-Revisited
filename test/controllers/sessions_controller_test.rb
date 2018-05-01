5079e0b733c2935bd0b52545db837e7fb0c5ba13
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-04-30 20:47:15 -0700

    Test Sessions controller action 'logout'

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,14 +1,14 @@
 require "test_helper"
 
 describe SessionsController do
+  before do
+    @user = User.new(username: 'thebatman')
+    @user.must_be :valid?
+    @user.save
+  end
 
   describe 'login' do
 
-    before do
-      @user = User.new(username: 'thebatman')
-      @user.must_be :valid?
-      @user.save
-    end
 
     it 'responds with success for an extant user' do
       username = @user.username
@@ -45,7 +45,17 @@ describe SessionsController do
 
   end # login
 
-  describe 'logout' do 
+  describe 'logout' do
+    before do
+      post login_path, params: { username: @user.username }
+    end
+    it 'responds with success' do
+      session[:user_id].must_equal @user.id
+      post logout_path
+      must_respond_with :redirect
+      must_redirect_to root_path
+      session[:user_id].must_be_nil
+    end
   end # logout
 
 end # SessionsController
