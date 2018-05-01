15eb02aa9eda00157c6a8f6e3ff23d6064ccdf91
Jackie <jonofre16@hotmail.com>
2018-04-30 21:54:33 -0700

    Added user controller tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,17 @@
 class UsersController < ApplicationController
-  # def index
-  #   @users = User.all
-  # end
-  #
-  # def show
-  #   @user = User.find_by(id: params[:id])
-  #   render_404 unless @user
-  # end
   before_action :find_user
+
+  def index
+    if @login_user.nil?
+      render_404
+    else
+      @users = User.all
+    end
+  end
 
   def show
     @user = User.find_by(id: params[:id])
+    render_404 unless @login_user
   end
 
-  def index
-    @users = User.all
-  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,4 +2,30 @@ require 'test_helper'
 
 describe UsersController do
 
+    describe 'index' do
+      it "should get index for signed in users" do
+        perform_login(users(:dan))
+        get users_path
+        must_respond_with :success
+      end
+
+      it "should render 404 for guest users" do
+        get users_path
+        must_respond_with :missing
+      end
+    end
+
+    describe 'show' do
+      it "should get show for signed in users" do
+        perform_login(users(:dan))
+        get user_path(users(:kari))
+        must_respond_with :success
+      end
+
+      it "should render 404 for guest users" do
+        get user_path(users(:kari))
+        must_respond_with :missing
+      end
+    end
+
 end
