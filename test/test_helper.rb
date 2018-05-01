9947991401cb6d54224d7994b8d71f6fe8ba36ac
Nora Peters <norapeters5@gmail.com>
2018-04-30 19:46:17 -0700

    refactors perform_login method in test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -44,9 +44,10 @@ class ActiveSupport::TestCase
     }
   end
 
+
   def perform_login(user)
     OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
-
-    get auth_callback_path(user.provider)
+    get auth_callback_path(:github)
   end
+
 end
