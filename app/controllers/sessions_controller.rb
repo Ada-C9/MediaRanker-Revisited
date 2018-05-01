c2b96500c761052a062ae2536c6aeaafcd6ac1ed
Brenda Rios <riosbrenda@gmail.com>
2018-05-01 00:02:07 -0700

    sessions controller refactored and added new ad destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,20 +11,32 @@ class SessionsController < ApplicationController
       if @user.nil?
 
         @user = User.get_from_github(auth_hash)
+        successfull_save = @user.save
+
+        if successfull_save
+          flash[:status] = :success
+          flash[:result_text] = "Logged in successfully"
+          session[:user_id] = @user.id
+
+          redirect_to root_path
+        else
+          flash[:result_text] = "Could not log in"
+          redirect_to root_path
+
+        end
 
       else
+
         flash[:status] = :success
         flash[:result_text] = "Logged in successfully"
-
+        session[:user_id] = @user.id
+        redirect_to root_path
       end
-      session[:user_id] = @user.id
-
     else
-
-      flash[:result_text] = "Could not log in"
-
+      flash[:error] = "Logging in through GitHub not successful"
+      redirect_to root_path
     end
-    redirect_to root_path
+
   end
 
   def destroy
@@ -32,6 +44,22 @@ class SessionsController < ApplicationController
     flash[:status] = :success
     flash[:result_text] = "Successfully logged out"
     redirect_to root_path
-    
+
   end
+
+  def new
+    @user = User.new
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out"
+    redirect_to root_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+ end
 end
