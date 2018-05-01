41e1329278068260c63bd76cd5c956f895acac11
Hannah Cameron <hannahiscoding@gmail.com>
2018-04-30 22:28:51 -0700

    wrote user controller tests, updated rails version in gemfile

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,46 @@
 require 'test_helper'
 
 describe UsersController do
+  describe 'index' do
+    it 'responds with success when users exist' do
+      User.count.must_be :>, 0
+
+      get users_path
+
+      must_respond_with :success
+    end
+
+    it 'responds with success when no users' do
+
+      User.all.each do |user|
+        user.delete
+      end
+
+      User.count.must_equal 0
+
+      get users_path
+
+      must_respond_with :success
+    end
+
+  end
+
+  describe 'show' do
+      it 'responds with success when user exists' do
+        user = User.first
+
+        get user_path(user)
+
+        must_respond_with :success
+      end
+
+      it 'render not_found when user exists' do
+        user_id = User.last.id + 1
+
+        get user_path(user_id)
+
+        must_render :not_found
+      end
+  end
+
 end
