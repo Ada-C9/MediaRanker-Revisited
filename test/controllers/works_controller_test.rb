52dc580ada4ea233ddc6211a6473fbb86461ef52
Lily Sky <lilyxsky@gmail.com>
2018-04-30 17:44:08 -0700

    tests pass except related to work upvotes

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -28,7 +28,6 @@ describe WorksController do
       Work.count.must_equal 0
       get root_path
       must_respond_with :success
-
     end
   end
 
@@ -36,36 +35,80 @@ describe WorksController do
   INVALID_CATEGORIES = ["nope", "42", "", "  ", "albumstrailingtext"]
 
   describe "index" do
-    it "succeeds when there are works" do
+    it "succeeds for OATH users when there are works" do
+      perform_login(users(:ada))
+      Work.count.must_equal 4
+      get works_path
+
+      must_respond_with :success
+    end
+
+    it "succeeds for users when there are works" do
+      post login_path, params: {
+        username: "dan"
+      }
       Work.count.must_equal 4
       get works_path
       must_respond_with :success
     end
 
-    it "succeeds when there are no works" do
+    it "succeeds for OATH users when there are no works" do
+      perform_login(users(:ada))
       Work.all.each do |work|
         work.destroy
       end
-
       Work.count.must_equal 0
       get works_path
+
       must_respond_with :success
+    end
+
+    it "succeeds for users when there are no works" do
+      post login_path, params: {
+        username: "dan"
+      }
+      Work.all.each do |work|
+        work.destroy
+      end
+      Work.count.must_equal 0
+      get works_path
+
+      must_respond_with :success
+    end
+
+    it "redirects to root_path if not logged in" do
+      Work.count.must_equal 4
+      get works_path
+      must_redirect_to root_path
     end
   end
 
   describe "new" do
-    it "succeeds" do
+    it "succeeds for OATH users" do
+      perform_login(users(:ada))
       get new_work_path
+
       must_respond_with :success
+    end
+
+    it "succeeds for logged in users" do
+      post login_path, params: {
+        username: "dan"}
+      get new_work_path
+
+      must_respond_with :success
+    end
+
+    it "redirects for no users" do
+      get new_work_path
+      must_redirect_to root_path
     end
   end
 
   describe "create" do
-    it "creates a work with valid data for a real category" do
+    it "creates a work with valid data for a real category - OATH" do
       proc {
-        post login_path, params: {
-          username: "dan"
-        }
+        perform_login(users(:ada))
 
         post works_path, params: {
           work: {
@@ -81,8 +124,29 @@ describe WorksController do
       must_redirect_to work_path((Work.find_by(title: "Book title").id))
     end
 
-    it "renders bad_request and does not update the DB for bogus data" do
+    it "creates a work with valid data for a real category - User" do
       proc {
+        post login_path, params: {
+          username: "dan"}
+
+        post works_path, params: {
+          work: {
+            title: "Book title",
+            creator: "An author",
+            category: "Book",
+            publication_year: 1984
+          }
+        }
+      }.must_change "Work.count", 1
+
+      must_respond_with :redirect
+      must_redirect_to work_path((Work.find_by(title: "Book title").id))
+    end
+
+    it "renders bad_request; doesn't update the DB for bogus data - OATH" do
+      proc {
+        perform_login(users(:ada))
+
         post works_path, params: {
           work: {
             title: "",
@@ -97,9 +161,29 @@ describe WorksController do
       must_respond_with :bad_request
     end
 
-    it "renders 400 bad_request for bogus categories" do
+    it "renders bad_request; doesn't update the DB for bogus data - User" do
+      proc {
+        post login_path, params: {
+          username: "dan"}
+
+        post works_path, params: {
+          work: {
+            title: "",
+            creator: "An author",
+            category: "Book",
+            publication_year: 1984
+          }
+        }
+      }.must_change "Work.count", 0
+
+      Work.count.must_equal 4
+      must_respond_with :bad_request
+    end
+
+    it "renders 400 bad_request for bogus categories - OATH" do
       INVALID_CATEGORIES.each do |category|
         proc {
+          perform_login(users(:ada))
           post works_path, params: {
             work: {
               title: "",
@@ -114,107 +198,247 @@ describe WorksController do
         must_respond_with :bad_request
       end
     end
+
+    it "renders 400 bad_request for bogus categories - User" do
+      INVALID_CATEGORIES.each do |category|
+        proc {
+          post login_path, params: {
+            username: "dan"}
+          post works_path, params: {
+            work: {
+              title: "",
+              creator: "An author",
+              category: category,
+              publication_year: 1984
+            }
+          }
+        }.must_change "Work.count", 0
+
+        Work.count.must_equal 4
+        must_respond_with :bad_request
+      end
+    end
+
+    it "redirects for no user" do
+      proc {
+        post works_path, params: {
+          work: {
+            title: "Book title",
+            creator: "An author",
+            category: "Book",
+            publication_year: 1984
+          }
+        }
+      }.must_change "Work.count", 0
+
+      must_respond_with :redirect
+      must_redirect_to root_path
+    end
   end
 
   describe "show" do
-    it "succeeds for an extant work ID" do
+    it "succeeds for an extant work ID - OATH" do
+      perform_login(users(:ada))
       get work_path(works(:album).id)
-      must_respond_with :found
+
+      must_respond_with :success
     end
 
-    it "renders 404 not_found for a bogus work ID" do
+    it "succeeds for an extant work ID - User" do
+      post login_path, params: {
+        username: "dan"}
+      get work_path(works(:album).id)
+
+      must_respond_with :success
+    end
+
+    it "renders 404 not_found for a bogus work ID - OATH" do
+      perform_login(users(:ada))
+
+      get work_path(Work.last.id+1)
+      must_respond_with :not_found
+    end
+
+    it "renders 404 not_found for a bogus work ID -  User" do
       post login_path, params: {
         username: "dan"
       }
-      
+
       get work_path(Work.last.id+1)
       must_respond_with :not_found
+    end
+
+    it "redirects when user not logged in" do
+      get work_path(works(:album).id)
+      must_redirect_to root_path
     end
   end
 
   describe "edit" do
-    it "succeeds for an extant work ID" do
+    it "succeeds for an extant work ID - OATH" do
+      perform_login(users(:ada))
       get edit_work_path(works(:album).id)
+
       must_respond_with :success
     end
 
-    it "renders 404 not_found for a bogus work ID" do
+    it "succeeds for an extant work ID - User" do
+      post login_path, params: {
+        username: "dan"}
+      get edit_work_path(works(:album).id)
+
+      must_respond_with :success
+    end
+
+    it "renders 404 not_found for a bogus work ID - OATH" do
+      perform_login(users(:ada))
       get edit_work_path(Work.last.id+1)
-      must_redirect_to :root_path
+
+      must_respond_with :not_found
+    end
+
+    it "logged in: renders 404 not_found for a bogus work ID - User" do
+      post login_path, params: {
+        username: "dan"}
+      get edit_work_path(Work.last.id+1)
+
+      must_respond_with :not_found
+    end
+
+    it "redirects for an extant work ID when no user" do
+      get edit_work_path(works(:album).id)
+      must_redirect_to root_path
+    end
+
+    it "redirects for a bogus work ID when no user" do
+      get edit_work_path(Work.last.id+1)
+      must_redirect_to root_path
     end
   end
 
   describe "update" do
-    it "succeeds for valid data and an extant work ID" do
+    it "succeeds for valid data and an extant work ID - OATH" do
+      perform_login(users(:ada))
+
       updated_title = "deeface"
 
       put work_path(works(:album).id), params: {
         work: {
-          title: "deeface"
-        }
-      }
+          title: "deeface" }}
 
       updated_work = Work.find(works(:album).id)
       updated_work.title.must_equal updated_title
-      must_respond_with :redirect
+      must_redirect_to work_path(works(:album).id)
     end
 
-    it "renders bad_request for bogus data" do
+    it "succeeds for valid data and an extant work ID - User" do
+      post login_path, params: {
+      username: "dan"}
+
+      updated_title = "deeface"
+
       put work_path(works(:album).id), params: {
         work: {
-          title: ""
-        }
-      }
+          title: "deeface" }}
 
+      updated_work = Work.find(works(:album).id)
+      updated_work.title.must_equal updated_title
+      must_redirect_to work_path(works(:album).id)
+    end
+
+    it "renders bad_request for bogus data - OATH" do
+      perform_login(users(:ada))
+      put work_path(works(:album).id), params: {
+        album: {
+          title: "" }}
+
+      must_respond_with :bad_request
+    end
+
+    it "renders bad_request for bogus data - User" do
+      post login_path, params: {
+        username: "dan"}
+      put work_path(works(:album).id), params: {
+        album: {
+          title: "" }}
+
+      must_respond_with :bad_request
+    end
+
+    it "renders 404 not_found for a bogus work ID - OATH" do
+      perform_login(users(:ada))
+
+      put work_path(Work.last.id+1), params: {
+        work: {
+          title: "deeface" }}
       must_respond_with :not_found
     end
 
-    it "renders 404 not_found for a bogus work ID" do
-      put work_path("works(:album).id"), params: {
+    it "renders 404 not_found for a bogus work ID - User" do
+      post login_path, params: {
+      username: "dan"}
+
+      put work_path(Work.last.id+1), params: {
         work: {
-          category: "deeface"
-        }
-      }
+          title: "deeface" }}
       must_respond_with :not_found
+    end
+
+    it "redirects when not logged in" do
+      put work_path(works(:album).id), params: {
+        work: {
+          title: "deeface" }}
+
+      must_redirect_to root_path
     end
   end
 
   describe "destroy" do
-    it "succeeds for an extant work ID" do
+    it "succeeds for an extant work ID - User" do
+      Work.count.must_equal 4
+      post login_path, params: {
+      username: "dan"}
       delete work_path(works(:album).id)
+
       must_respond_with :redirect
       must_redirect_to root_path
+      Work.count.must_equal 3
     end
 
     it "renders 404 not_found and does not update the DB for a bogus work ID" do
+      post login_path, params: {
+      username: "dan"}
       delete work_path("carrot")
+
       must_respond_with :not_found
+    end
+
+    it "redirects if not logged in" do
+      Work.count.must_equal 4
+      delete work_path(works(:album).id)
+
+      must_respond_with :redirect
+      must_redirect_to root_path
+      Work.count.must_equal 4
     end
   end
 
   describe "upvote" do
-    it "redirects to the work page if no user is logged in" do
-      Vote.count.must_equal 3
+    it "succeeds for a logged-in user and a fresh user-vote pair - OATH" do
+      proc {
+        perform_login(users(:ada))
 
-      post upvote_path(works(:album).id)
-
-      must_respond_with :redirect
-      # must_redirect_to work_path(works(:album).id)
-      Vote.count.must_equal 3
-    end
-
-    it "redirects to the work page after the user has logged out" do
-      post logout_path, params: {
-        user: {
-          username: "dan"
+        post upvote_path(works(:poodr).id), params: {
+          user: users(:ada),
+          work: works(:poodr)
         }
-      }
+      }.must_change "Vote.count", 1
 
       must_respond_with :redirect
-      must_redirect_to root_path
+      must_redirect_to work_path(works(:poodr).id)
     end
 
-    it "succeeds for a logged-in user and a fresh user-vote pair" do
+    it "succeeds for a logged-in user and a fresh user-vote pair - User" do
       proc {
         post login_path, params: {
           username: "dan"
@@ -230,13 +454,24 @@ describe WorksController do
       must_redirect_to work_path(works(:poodr).id)
     end
 
-    it "redirects to the work page if the user has already voted for that work" do
+    # it "redirects to the work page if the user has already voted for that work - OATH" do
+    #   proc {
+    #     perform_login(users(:ada))
+    #
+    #     post upvote_path(works(:album).id), params: {
+    #       user: users(:ada),
+    #       work: works(:album)
+    #     }
+    #   }.must_change "Vote.count", 0
+    #   # must_respond_with :redirect
+    #   # must_redirect_to work_path(works(:album).id)
+    # end
 
+    it "redirects to the work page if the user has already voted for that work - User" do
       proc {
         post login_path, params: {
           username: "dan"
         }
-
         post upvote_path(works(:album).id), params: {
           user: users(:dan),
           work: works(:album)
@@ -244,7 +479,15 @@ describe WorksController do
       }.must_change "Vote.count", 0
       must_respond_with :redirect
       must_redirect_to work_path(works(:album).id)
+    end
 
+    it "redirects to the work page if no user is logged in" do
+      Vote.count.must_equal 3
+      post upvote_path(works(:album).id)
+    
+      must_respond_with :redirect
+      must_redirect_to root_path
+      Vote.count.must_equal 3
     end
   end
 end
