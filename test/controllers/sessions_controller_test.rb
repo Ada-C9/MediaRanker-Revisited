b0964c36b38734cdf6720225bba6ce4cf0b31e27
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-05-01 02:25:53 -0700

    added destory session test

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -70,18 +70,14 @@ describe SessionsController do
 		end
 	end
 
-	describe "destory" do
+	describe "destroy" do
+		user = users(:dan)
+
+		login_for_test(user)
+		logout_for_test(user)
+
+		must_redirect_to logout_path
 
 	end
-
-	# # proc {
-	#
-	# 	post login_path, params: { username: users(:kari).username }
-	#
-	# 	voted_on_work = Work.find_by(id: works(:movie).id)
-	# 	post upvote_path(voted_on_work.id), params: {
-	# 		vote: { user: users(:kari), work: voted_on_work }
-	# 	}
-	# # }.must_change 'Vote.count', 1
 
 end
