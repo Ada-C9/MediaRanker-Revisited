c9557a414aa43f1adc0a09abf07a19d6f127bb54
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-04-30 20:40:15 -0700

    commented out session previous test

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,23 +1,23 @@
 require "test_helper"
 
 describe SessionsController do
-  let (:user) { users(:kari) }
-  it "login_form" do
-    get login_path
-    must_respond_with :success
-  end
-  it "can log in" do
-    post login_path, params: {
-          username: "kari"
-        }
-
-    must_redirect_to root_path
-  end
-
-  it "can log out" do
-    post logout_path
-    must_redirect_to root_path
-
-  end
-
+  # let (:user) { users(:kari) }
+  # it "login_form" do
+  #   get login_path
+  #   must_respond_with :success
+  # end
+  # it "can log in" do
+  #   post login_path, params: {
+  #         username: "kari"
+  #       }
+  #
+  #   must_redirect_to root_path
+  # end
+  #
+  # it "can log out" do
+  #   post logout_path
+  #   must_redirect_to root_path
+  #
+  # end
+  #
 end
