e764b0caf58a91d1e281cd48afa5a28d4fd966d5
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-04-30 17:07:56 -0700

    All tests passing, including legacy model tests, which had to be adjusted for new Oauth situation, and email validations for user.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -28,12 +28,12 @@ describe User do
 
     it "requires a unique username" do
       username = "test username"
-      user1 = User.new(username: username)
+      user1 = User.new(username: username, email: 'user@usermail.com' )
 
       # This must go through, so we use create!
       user1.save!
 
-      user2 = User.new(username: username)
+      user2 = User.new(username: username, email: 'twoser@usermail.com')
       result = user2.save
       result.must_equal false
       user2.errors.messages.must_include :username
