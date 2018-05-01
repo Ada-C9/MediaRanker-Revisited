2c946f24be3d4e8941082fb986daa2458419a7f6
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-04-30 20:27:48 -0700

    add sessions controller tests and omni auth

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,4 +2,18 @@ require "test_helper"
 
 describe SessionsController do
 
+  describe "login" do
+    it 'succeeds at finding login path' do
+      get login_path
+      must_respond_with :success
+    end
+  end
+
+  describe "logout" do
+    it 'succeeds at if no user' do
+      get logout_path
+      must_respond_with :success
+    end
+  end
+
 end
