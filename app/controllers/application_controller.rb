71888737c9545cdc7a46cadd6feb95e02c11a51c
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-05-01 01:38:49 -0700

    cleaned out previous code/terrible ideas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,25 +20,4 @@ private
     @login_user = User.find_by(id: session[:user_id])
   end
 
-  # def require_login
-  #   if @login_user.nil?
-  #   # if current_user.nil?
-  #     flash[:error] = "You must be logged in to view this section"
-  #     redirect_to session_path
-  #   end
-  # end
-
-
-  # # Check that the user is logged in, and send back
-  # # an error if they are not
-  # # Store the looked-up user in @user in case
-  # # someone wants to use it later.
-  # # We write this method here so it will be available
-  # # in _all_ controllers
-  # def require_login
-  #   @user = User.find_by(id: session[:user_id])
-  #   head :unauthorized unless @user
-  # end
-
-
 end
