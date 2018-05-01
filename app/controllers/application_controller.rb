4b4dd2e1e3f2c0349fd00f198d978f986d72a82b
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-04-30 17:17:26 -0700

    render 404 working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :find_user
+  before_action :find_user, except: [:root]
 
   def render_404
     # DPR: this will actually render a 404 page in production
@@ -10,8 +10,33 @@ class ApplicationController < ActionController::Base
 
 private
   def find_user
-    if session[:user_id]
+    # if session[:user_id]
+    # raise
       @login_user = User.find_by(id: session[:user_id])
-    end
+      render_404 unless @login_user
+      # head :unauthorized unless @user
+    # end
   end
+
+  # def require_login
+  #   if @login_user.nil?
+  #   # if current_user.nil?
+  #     flash[:error] = "You must be logged in to view this section"
+  #     redirect_to session_path
+  #   end
+  # end
+
+
+  # # Check that the user is logged in, and send back
+  # # an error if they are not
+  # # Store the looked-up user in @user in case
+  # # someone wants to use it later.
+  # # We write this method here so it will be available
+  # # in _all_ controllers
+  # def require_login
+  #   @user = User.find_by(id: session[:user_id])
+  #   head :unauthorized unless @user
+  # end
+
+
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,21 +3,40 @@ require 'test_helper'
 describe UsersController do
 	describe "auth_callback" do
 	 it "logs in an existing user and redirects to the root route" do
-		 start_count = User.count
-		 user = users(:dan)
+			start_count = User.count
+			user = users(:dan)
 
-		 puts mock_auth_hash(user).inspect
-		 OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
-		 get auth_callback_path(:github)
+			login(user)
+			must_redirect_to root_path
+			session[:user_id].must_equal user.id
 
-		 must_redirect_to root_path
+			# Should *not* have created a new user
+			User.count.must_equal start_count
 
-		 session[:user_id].must_equal user.id
-
-		 User.count.must_equal start_count
+		 ## longer way before shortcut
+		 # start_count = User.count
+		 # user = users(:dan)
+		 #
+		 # OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+		 # get auth_callback_path(:github)
+		 #
+		 # must_redirect_to root_path
+		 # session[:user_id].must_equal user.id
+		 # User.count.must_equal start_count
 	 end
 
 	 it "creates an account for a new user and redirects to the root route" do
+		 	start_count = User.count
+		  user = User.new(provider: "github", uid: 99999, username: "test_user", email: "test@user.com")
+
+		  OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+		  get auth_callback_path(:github)
+
+		  must_redirect_to root_path
+
+		  User.count.must_equal start_count + 1
+
+		  session[:user_id].must_equal User.last.id
 	 end
 
 	 it "redirects to the login route if given invalid user data" do
