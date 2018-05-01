6f84c455651b023b7cae9771d5225c0c9a780ee0
Brenda Rios <riosbrenda@gmail.com>
2018-05-01 00:38:55 -0700

    Simplified method that gets info from github in user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,23 +5,11 @@ class User < ApplicationRecord
   validates :username, uniqueness: true, presence: true
 
   def self.get_from_github(auth_hash)
-    user = User.new(
-      username: auth_hash['info']['name'],
-      uid: auth_hash['uid'],
-      email: auth_hash['info']['email'],
-      provider: auth_hash['provider'])
-
-      if user.save
-        flash[:status] = :success
-        flash[:result_text] = "#{user.username}Logged in successfully"
-
-      else
-        flash.now[:status] = :failure
-        flash.now[:result_text] = "Could not log in"
-        flash.now[:messages] = user.errors.messages
-      end
-
-      return user
-
+    return User.new(
+        provider: auth_hash[:provider],
+        uid: auth_hash[:uid],
+        email: auth_hash[:info][:email],
+        username: auth_hash[:info][:nickname]
+      )
     end
   end
