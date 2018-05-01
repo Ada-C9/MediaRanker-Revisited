d6fac34bbe2ac77d75764263590a646f181fd3b8
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-05-01 14:14:05 -0700

    Authorize guests to view only the main page with spotlight and top tens

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,9 @@ private
       @login_user = User.find_by(id: session[:user_id])
     end
   end
+
+  def require_login
+    @user = User.find_by(id: session[:user_id])
+    head :unauthorized unless @user
+  end
 end
